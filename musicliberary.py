7a1a0aafdb02f1cd9acd39fef381040dc2e115d6
Anmol7860-bit <anmolbhandare7860@gmail.com>
2025-09-09 13:09:31 +0530

    installed music liberary and open ai

---
+++ musicliberary.py
@@ -0,0 +1,7 @@
+music={ 
+    'space night':'https://youtu.be/GoWG2Hx3F98?si=rm7ybWO_aDY89j5j',
+    'humnasheen':'https://youtu.be/SqBf1hak1eg?si=yRGm6bFE_qUDAT_d',
+    'no cap': 'https://youtu.be/PejQbGZraqg?si=8b-Ew3YuKvvHkwS4',
+    'jimmy cooks':'https://youtu.be/4nyld2SqleU?si=xTlQWZQ8_sP2FrMu',
+    'espresso':'https://youtu.be/eVli-tstM5E?si=JdP0NSH-w4Vcgxhw'
+}
